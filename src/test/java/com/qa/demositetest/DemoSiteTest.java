55f39f06af727f3e2364b82d0e826bfde4692236
shivalasvegas <shiva.king@academytrainee.com>
2020-08-04 13:06:16 +0100

    SK - DemoSiteTest.java: modified for test pass

--- src/test/java/com/qa/demositetest/DemoSiteTest.java
+++ src/test/java/com/qa/demositetest/DemoSiteTest.java
@@ -1,5 +1,7 @@
 package com.qa.demositetest;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,38 +10,62 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//Create a user; log in with that user, assert that it was successful.
+//The website only stores one user and password at a time so you must be quick in your automation.
+
 public class DemoSiteTest {
-	
-private WebDriver driver;
-	
+
+	private WebDriver driver;
+
 	@Before
 	public void init() {
-		System.setProperty("webdriver.chrome.driver", "C://Users//shiva//Documents//AA_PROJECTS//DG_seleniumProjects//seleniumTest1//src//test//resources//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C://Users//shiva//Documents//AA_PROJECTS//DG_seleniumProjects//seleniumTest1//src//test//resources//chromedriver.exe");
 		driver = new ChromeDriver();
-		//driver.manage().window().fullscreen();
-		
-			
+		// driver.manage().window().fullscreen();
+
 	}
-	
+
 	WebElement target;
-	
+
 	@Test
-	public void test1() throws InterruptedException{
+	public void test1() throws InterruptedException {
 		driver.get("http://thedemosite.co.uk/");
+
+		target = driver.findElement(
+				By.xpath("/html/body/div[1]/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]"));
+		target.click();
 		
-//target=driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input"));
-//target.sendKeys("kittens");
-//target.submit();
-//target=driver.findElement(By.id("hdtb-msb-vis"));  //videos
-//target.click();
+		target = driver.findElement(By.name("username"));
+		target.sendKeys("ArthurDent");
 		
-		Thread.sleep(5000);
+		target = driver.findElement(By.name("password"));
+		target.sendKeys("earthToGargle");
+		
+		target = driver.findElement(By.name("FormsButton2"));
+		target.click();
+		
+		target = driver.findElement(
+				By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[2]/small/a"));
+		target.click();
+		
+		target = driver.findElement(By.name("username"));
+		target.sendKeys("ArthurDent");
+		
+		target = driver.findElement(By.name("password"));
+		target.sendKeys("earthToGargle");
+		
+		target = driver.findElement(By.name("FormsButton2"));
+		target.click();
+		
+		assertTrue(driver.getPageSource().contains("**Successful Login**"));
+		
+		Thread.sleep(9000);
 	}
-	
+
 	@After
 	public void tearDown() {
 		driver.close();
 	}
-
 
 }
